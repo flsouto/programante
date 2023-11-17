964de4bfffd30c0ae4e2e1151a707f999a442c2c
madrug4 <madrug4@gmail.com>
2023-11-17 12:40:18 -0300

    cria gitignore e atualiza mkreadme

--- mkreadme.py
+++ mkreadme.py
@@ -4,13 +4,22 @@ import re
 
 readmes = glob.glob("*/README.md")
 
-result = ""
+groups = {}
 
 for r in readmes:
     lang = r.split('-')[0]
+    if lang == 'php8': lang = 'php'
+    if not lang in groups: groups[lang] = ""
     text = Path(r).read_text()
     match = re.search(r'#(.*)',text)
     if not match or not '?' in match.group(1):
         print("This post does not contain question mark in the heading: "+r)
         continue
-    result += "## "+match.group(1)+"?"
+    groups[lang] += "### "+match.group(1)+"\n[Veja a resposta]("+r+") \n"
+
+result = "# Questões para os amantes de programação\n"
+for lang in groups:
+    result += "## Questões de "+lang+"\n"
+    result += groups[lang]
+
+Path("README.md").write_text(result)
