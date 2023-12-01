7000460e725cee65e0e80f8d5ed49683b1b03b11
nathanfinger <nathanfinger@gmail.com>
2023-12-01 17:33:52 -0300

    add script to add new tutorials

---
+++ add.py
@@ -0,0 +1,29 @@
+import slugify
+from pathlib import Path
+from stop_words import get_stop_words
+
+title = input("title:")
+lang, title = title.split(':')
+lang = lang.lower()
+filtered_words = [word for word in title.split(' ') if word not in get_stop_words('portuguese')]
+slug = lang + '-' + slugify.slugify(' '.join(filtered_words))
+path = Path(slug)
+if not path.exists(): path.mkdir()
+
+import sys
+print("content:")
+content = "#{}\n".format(title.title()) + sys.stdin.read();
+
+readmef = path / "README.md"
+with open(readmef, "w") as f:
+    f.write(content)
+
+from subprocess import check_output
+def run(*args):
+    print(checkout_output(args))
+
+run("python3", "mkreadme.py")
+run("git","add",readmef)
+run("git","add","README.md")
+run("git","commit","-m","Add tutorial: "+title)
+run("git","push","origin","master")
