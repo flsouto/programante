19ab7b45980171d2012d64a6d591beb7487d9ae0
flsouto666 <flsouto666@gmail.com>
2023-12-12 17:09:24 -0300

    improvements

---
+++ check.py
@@ -0,0 +1,22 @@
+from glob import glob
+from sys import argv
+from subprocess import check_output
+
+def run(*args):
+    print(check_output(args))
+
+pending = glob("*/pending")
+
+if not argv[1]:
+
+    for i,f in enumerate(pending):
+        print("{} - {}".format(i, f))
+else:
+    run(["rm",pending[i]])
+    run(["git","add",pending[i]])
+    run(["git","commit", "--author", "flsouto <flsouto666@gmail.com>", "-m", "resolve"])
+
+    i = int(argv[1])
+    url = "https://github.com/flsouto/programante/tree/master/"
+    url+= pending[i].split("/")[0]
+    print(url)
